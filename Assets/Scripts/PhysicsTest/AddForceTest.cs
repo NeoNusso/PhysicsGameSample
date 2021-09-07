8cd6fd4ba15b73a31766bab22d55977a00452e69
DESKTOP-E5PTARA\onuss <o.nusso@gmail.com>
2021-09-08 00:49:43 +0900

    forcemode

--- Assets/Scripts/PhysicsTest/AddForceTest.cs
+++ Assets/Scripts/PhysicsTest/AddForceTest.cs
@@ -4,16 +4,17 @@ using UnityEngine;
 
 public class AddForceTest : MonoBehaviour
 {
-
+    public Vector3 _force = new Vector3(0.0f, 0.0f, 1.0f);
     public Rigidbody _rigid;
 
+    public ForceMode _forceMode;
     float _time = 1.0f;
     void FixedUpdate()
     {
         if (_time > 0.0f)
         {
             _time -= Time.fixedDeltaTime;
-            _rigid.AddForce(new Vector3(0.0f, 0.0f, 1.0f), ForceMode.Force);
+            _rigid.AddForce(_force, _forceMode);
         }
     }
 }
--- Assets/Scripts/PhysicsTest/AddTorqueByForce.cs
+++ Assets/Scripts/PhysicsTest/AddTorqueByForce.cs
@@ -16,11 +16,13 @@ namespace Nussoft
 			if (_time > 0.0f)
 			{
 				_time -= Time.fixedDeltaTime;
-				var center = _body.worldCenterOfMass;
+				var center = _body.transform.position;
 				var pos1 = center;
-				pos1.x -= 10.0f;
+				var pos2 = center;
+				pos1.x -= 1.0f;
+				pos2.x += 1.0f;
 				_body.AddForceAtPosition(new Vector3(0.0f, 0.0f, _force), pos1);
-				
+				_body.AddForceAtPosition(new Vector3(0.0f, 0.0f, -_force), pos2);
 			}
 		}
 	}
